ac7036067b101ba5569b306cd6579827ff16337f
monkey0722 <monkeys7222@gmail.com>
2023-11-11 20:09:49 +0900

    feat: Implement object conversion extensions

--- app/src/main/java/com/example/ktunsplashapp/data/remote/SearchPhotosResultDto.kt
+++ app/src/main/java/com/example/ktunsplashapp/data/remote/SearchPhotosResultDto.kt
@@ -1,6 +1,7 @@
 package com.example.ktunsplashapp.data.remote
 
 
+import com.example.ktunsplashapp.domain.models.Photo
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
@@ -11,3 +12,19 @@ data class SearchPhotosResultDto(
     @Json(name = "total_pages")
     val totalPages: Int?
 )
+
+fun SearchPhotosResultDto.toPhotos(): List<Photo> {
+    return results?.mapNotNull { result ->
+        if (result.id != null && result.urls?.raw != null) {
+            Photo(
+                photoId = result.id,
+                description = result.description,
+                likes = result.likes,
+                imageUrl = result.urls.raw,
+                photographer = result.user?.username
+            )
+        } else {
+            null
+        }
+    } ?: emptyList()
+}

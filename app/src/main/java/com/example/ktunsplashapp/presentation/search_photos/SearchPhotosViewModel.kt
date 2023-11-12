f68ec9bcd31375b4fc64135e6d68bd33a948071d
monkey0722 <monkeys7222@gmail.com>
2023-11-12 12:41:31 +0900

    feat(SearchPhotosView): Add state and model

---
+++ app/src/main/java/com/example/ktunsplashapp/presentation/search_photos/SearchPhotosViewModel.kt
@@ -0,0 +1,56 @@
+package com.example.ktunsplashapp.presentation.search_photos
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ktunsplashapp.domain.use_cases.SearchPhotosUseCase
+import com.example.ktunsplashapp.utils.NetworkResponse
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
+
+@HiltViewModel
+class SearchPhotosViewModel @Inject constructor(
+    private val searchPhotosUseCase: SearchPhotosUseCase,
+) : ViewModel() {
+    private val _state = mutableStateOf(SearchPhotosState())
+    val state: State<SearchPhotosState> = _state
+
+    private var _query by mutableStateOf("programming")
+    val query: String get() = _query
+
+    init {
+        searchPhotos(_query)
+    }
+
+    fun onQueryChanged(queryString: String) {
+        _query = queryString
+    }
+
+    fun searchPhotos(query: String) {
+        _state.value = SearchPhotosState(isLoading = true)
+        searchPhotosUseCase(query).onEach { result ->
+            _state.value = when (result) {
+                is NetworkResponse.Success -> {
+                    SearchPhotosState(
+                        isLoading = false,
+                        photos = result.data ?: emptyList()
+                    )
+                }
+                is NetworkResponse.Failure -> {
+                    SearchPhotosState(
+                        isLoading = false,
+                        error = result.error ?: "An unexpected error occurred"
+                    )
+                }
+                is NetworkResponse.Loading -> {
+                    SearchPhotosState(isLoading = true)
+                }
+            }
+        }.launchIn(viewModelScope)
+    }
+}

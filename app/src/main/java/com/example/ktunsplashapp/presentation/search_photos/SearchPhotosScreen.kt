9ee12eb6333bc4757d9c320546d56c9ea94d2dd0
monkey0722 <monkeys7222@gmail.com>
2023-11-12 15:28:23 +0900

    feat: Add a SearchBar component

--- app/src/main/java/com/example/ktunsplashapp/presentation/search_photos/SearchPhotosScreen.kt
+++ app/src/main/java/com/example/ktunsplashapp/presentation/search_photos/SearchPhotosScreen.kt
@@ -1,19 +1,35 @@
 package com.example.ktunsplashapp.presentation.search_photos
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.ktunsplashapp.presentation.search_photos.components.PhotoThumbnail
+import com.example.ktunsplashapp.presentation.search_photos.components.SearchBar
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchPhotosScreen(
     viewModel: SearchPhotosViewModel = hiltViewModel(),
 ) {
     val state = viewModel.state.value
-    LazyColumn {
-        items(state.photos) { photo ->
-            PhotoThumbnail(photo = photo, onClick = {})
+    Scaffold(
+        topBar = {
+            SearchBar(
+                searchText = "",
+                onSearchTextChanged =  {},
+                onDone = {}
+            )
+        }
+    ) { paddingValue ->
+        LazyColumn(modifier = Modifier.padding(paddingValue)) {
+            items(state.photos) { photo ->
+                PhotoThumbnail(photo = photo, onClick = {})
+            }
         }
     }
 }
---
+++ app/src/main/java/com/example/ktunsplashapp/presentation/search_photos/components/SearchBar.kt
@@ -0,0 +1,77 @@
+package com.example.ktunsplashapp.presentation.search_photos.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
+@Composable
+fun SearchBar(
+    searchText: String,
+    onSearchTextChanged: (String) -> Unit,
+    onDone: () -> Unit,
+    placeholderText: String = "Search...",
+) {
+    var showClearButton by remember { mutableStateOf(false) }
+    val focusRequester = remember { FocusRequester() }
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    TopAppBar(title = {
+        OutlinedTextField(
+            value = searchText,
+            onValueChange = onSearchTextChanged,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 2.dp)
+                .onFocusChanged { focusState ->
+                    showClearButton = focusState.isFocused
+                }
+                .focusRequester(focusRequester),
+            placeholder = {
+                Text(text = placeholderText)
+            },
+            trailingIcon = {
+                IconButton(onClick = { onSearchTextChanged("") }) {
+                    Icon(
+                        imageVector = Icons.Filled.Close,
+                        contentDescription = "close",
+                    )
+                }
+            },
+            maxLines = 1,
+            singleLine = true,
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+            keyboardActions = KeyboardActions(onDone = {
+                keyboardController?.hide()
+                onDone()
+            })
+        )
+    })
+
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
+}
